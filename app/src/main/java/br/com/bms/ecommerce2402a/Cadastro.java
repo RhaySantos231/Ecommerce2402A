d8b0d40dc7bedd75848eff52805a791861e78dcf
Rhayssa <professorarhay.mbs@gmail.com>
2025-09-10 09:48:02 -0300

    Atualizar

--- app/src/main/java/br/com/bms/ecommerce2402a/Cadastro.java
+++ app/src/main/java/br/com/bms/ecommerce2402a/Cadastro.java
@@ -1,8 +1,15 @@
 package br.com.bms.ecommerce2402a;
 
+import static androidx.core.content.ContextCompat.startActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
 
 public class Cadastro extends AppCompatActivity {
 
@@ -24,7 +31,47 @@ public class Cadastro extends AppCompatActivity {
         editTelefoneCadastro = findViewById(R.id.editTelefoneCadastro);
         editSenhaCadastro = findViewById(R.id.editSenhaCadastro);
 
-
     }
+    
+    //08/09
+    private  void gerar_Cadastro(Usuario usuario){
+        FirebaseHelper.getAuth().createUserWithEmailAndPassword(usuario.getEmail(), usuario.getSenha())
+                .addOnCompleteListener(task -> {
+                    if(task.isSuccessful()){
+                        //Se a criação for bem-sucedida, abre a tela de login e finaliza a atual
+                        startActivity(new Intent(this, Login.class));
+                        finish();
+                        //recupera o id
+                        String id = task.getResult().getUser().getUid();
+                        usuario.setId(id);
+                        //Chama a função salvar dados
+                        usuario.salvar_dados();
+                        //Mostra mensagem de sucesso
+                        Toast.makeText(this, "Conta criada, tente fazer login", Toast.LENGTH_SHORT).show();
+                    }else{
+                        //se der erro, trata e exibe a mensagem para o usuario
+                        String erro = FirebaseHelper.validar_Erros(task.getException().getMessage());
+                        Toast.makeText(this, erro, Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+    //10/09
+    //Método para chamar ao clicar o botão de cadastro
+    public void validar_dados_cadastro(View view){
+        String nome = editNomeCadastro.getText().toString();
+        String email = editEmailCadastro.getText().toString();
+        String telefone = editTelefoneCadastro.getText().toString();
+        String senha = editSenhaCadastro.getText().toString();
 
+        //Validar os todos os campos de forma individual
+        if(!nome.isEmpty()){
+            if(!email.isEmpty()){
+                if (!telefone.isEmpty()){
+                    if(!senha.isEmpty()){
+                        //Se todos os campos estiverem
+                    }
+                }
+            }
+        }
+    }
 }
