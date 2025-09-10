a6874dd80e811fcc13705be3aaa465fd0f2f718e
Rhayssa <professorarhay.mbs@gmail.com>
2025-09-10 10:50:13 -0300

    Classe Usuario

--- app/src/main/java/br/com/bms/ecommerce2402a/Usuario.java
+++ app/src/main/java/br/com/bms/ecommerce2402a/Usuario.java
@@ -12,5 +12,14 @@ public class Usuario {
         DatabaseReference usuarioRef = FirebaseHelper.getDatabaseReference();
         usuarioRef.child("usuarios").child(this.getId()).setValue(id);
     }
-
+    public String getId(){return  id;}
+    public  void setId(String id){this.id = id;}
+    public String getNome(){return nome;}
+    public void setNome(String nome){this.nome = nome;}
+    public String getEmail(){return email;}
+    public  void setEmail(String email){this.email = email;}
+    public  String getTelefone(){return telefone;}
+    public  void setTelefone(String telefone){this.telefone = telefone;}
+    public String getSenha(){return  senha;}
+    public void setSenha(String senha){this.senha = senha;}
 }
